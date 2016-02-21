85af5f3b5a9e05e339dc64e1134d8916c153dcfa
drlastanao <drlastanao@gmail.com>
2016-02-21 11:02:16 +0100

    primera versión del buscador 21/02/2016

--- MarcadoresPersonal/MarcadoresPersonal/Principal.Designer.cs
+++ MarcadoresPersonal/MarcadoresPersonal/Principal.Designer.cs
@@ -28,20 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(454, 233);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "siguientes categorias:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "De alguna de las";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Contenga texto:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 251);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(454, 292);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultados";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(154, 71);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(279, 94);
+            this.checkedListBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(442, 214);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(214, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(485, 548);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Principal";
-            this.Text = "Form1";
+            this.Text = "Gestión Marcadores";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- MarcadoresPersonal/MarcadoresPersonal/Principal.cs
+++ MarcadoresPersonal/MarcadoresPersonal/Principal.cs
@@ -14,11 +14,115 @@ namespace MarcadoresPersonal
 {
     public partial class Principal : Form
     {
+        private NodoCentral n = new NodoCentral();
+
         public Principal()
         {
             InitializeComponent();
+
+
+            //borrar luego
+            Categoria c1 = new Categoria();
+            c1.nombre = "buscadores";
+            c1.descripcion = "buscadores de páginas";
+
+            Categoria c2 = new Categoria();
+            c2.nombre = "programación";
+            c2.descripcion = "páginas de programación";
+
+
+            Pagina p1 = new Pagina();
+            p1.Url = "www.google.es";
+            p1.categorias.Add(c1);
+            p1.Descripcion = "el mejor buscador";
+
+
+            Pagina p2 = new Pagina();
+            p2.Url = "www.udacity.com";
+            p2.categorias.Add(c2);
+            p2.Descripcion = "buenos cursos";
+
+
+
+
+
+            n.Categorias.Add(c1);
+            n.Categorias.Add(c2);
+
+            n.Paginas.Add(p1);
+            n.Paginas.Add(p2);
+
+
+
         }
 
-   
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<Pagina> aux = new List<Pagina>();
+
+
+            List<String> categoriasSeleccionadas = new List<String>();
+
+            int g = 0;
+            foreach (var item in checkedListBox1.CheckedIndices)
+            {
+
+                categoriasSeleccionadas.Add(checkedListBox1.Items[Convert.ToInt16(item)].ToString());
+            }
+
+
+            foreach (var item in n.Paginas)
+            {
+                Boolean seleccionar = true;
+                String texto=textBox1.Text.Trim() ;
+
+                if (texto!="")
+                {
+                    if (item.Url.Contains(texto) == false && item.Descripcion.Contains(texto) == false)
+                        seleccionar = false;
+                 }
+
+
+                if (categoriasSeleccionadas.Count>0)
+                {
+                    Boolean alguna = false;
+
+                    foreach (var item2 in item.categorias)
+                    {
+                        if (categoriasSeleccionadas.Contains(item2.nombre))
+                            alguna = true;
+                    }
+
+                    if (alguna == false) seleccionar = false;
+
+
+                }
+
+
+                if (seleccionar)
+
+                    aux.Add(item);                
+            }
+
+
+
+
+            dataGridView1.DataSource = aux;
+            
+                
+                    }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            List<String> cat = new List<String>();
+            foreach (var item in n.Categorias)
+            {
+                cat.Add(item.nombre);
+            }
+
+            checkedListBox1.Items.AddRange(cat.ToArray());
+
+
+        }
     }
 }
